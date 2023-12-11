8dca72beb3a1d1550e6506d8d07ae1380cd9cb31
eprintsman <eprincman@yandex.ru>
2023-12-11 21:41:53 +0300

    main_page->index

---
+++ Script_shopping_cart.js
@@ -0,0 +1,31 @@
+
+$('.minus_btn').click(function () {
+    $(this).click(minusKindOf($(this).attr('class').slice(-1)));
+});
+
+
+$('.plus_btn').click(function () {
+    $(this).click(plusKindOf($(this).attr('class').slice(-1)));
+});
+
+function minusKindOf(i) {
+    var N = parseInt($(".goods_quantity_" + i).text(), 10);
+    var total = parseInt($(".total_price_value").text().split(' ')[0], 10);
+    var price = parseInt($(".goods_coast_" + i).text().split(' ')[0], 10);
+
+    if (N > 0) {
+        $(".goods_quantity_" + i).text(N - 1);
+        $(".total_price_value").text(total - price + " P");
+    }
+}
+
+function plusKindOf(i) {
+    var N = parseInt($(".goods_quantity_" + i).text(), 10);
+    var total = parseInt($(".total_price_value").text().split(' ')[0], 10);
+    var price = parseInt($(".goods_coast_" + i).text().split(' ')[0], 10);
+
+    if (N < 10) {
+        $(".goods_quantity_" + i).text(N + 1);
+        $(".total_price_value").text(total + price + " P");
+    }
+}
